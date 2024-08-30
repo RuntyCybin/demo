26a7b242f731859ba12fcb5a10dd0910bc5819bd
m4st3 <runtycybin@gmail.com>
2024-08-30 09:57:33 +0200

    refactor of the login controller

--- src/main/java/com/cybinmvc/demo/LoginController.java
+++ src/main/java/com/cybinmvc/demo/LoginController.java
@@ -44,7 +44,8 @@ public class LoginController {
     public String showErrorPage(Model model) {
         // Comprobación de si el atributo "error" está presente en el modelo
         if (model.containsAttribute("err")) {
-            log.warn("warning", "Error Attribute Exists: " + model.getAttribute("error"));
+            log.warn("warning", "Error Attribute Exists: "
+                    + model.getAttribute("error"));
         } else {
             log.warn("warning", "Error Attribute NOT Found!");
         }
