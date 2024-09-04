433ed4745c0355460202b9fcf65bf026de71c5d4
m4st3 <runtycybin@gmail.com>
2024-09-04 17:06:25 +0200

    Test class initialized

--- src/test/java/com/cybinmvc/demo/DemoApplicationTests.java
+++ src/test/java/com/cybinmvc/demo/DemoApplicationTests.java
@@ -1,13 +1,56 @@
 package com.cybinmvc.demo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.cybinmvc.demo.model.UsersRepository;
+import com.cybinmvc.demo.model.Usuario;
+
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DemoApplicationTests {
 
+	@Autowired
+	UsersRepository repository;
+
+	@BeforeAll
+	void beforeAll() {
+		List<Usuario> lista = generarUsuarios();
+		repository.saveAll(lista);
+	}
+
+	private static List<Usuario> generarUsuarios() {
+		List<Usuario> usuarios = new ArrayList<>();
+
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setName("Paco");
+		usuario.setPwd("qwe");
+
+		usuarios.add(usuario);
+
+		return usuarios;
+	}
+
 	@Test
-	void contextLoads() {
+	void findByIdUserTestOK() {
+		Optional<Usuario> usuarioaux = repository.findById(1L);
+		assertTrue(usuarioaux.isPresent());
+	}
+
+	@AfterAll
+	void afterAll() {
+		repository.deleteAll();
 	}
 
 }
