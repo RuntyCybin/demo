617c9765a4d65a027038f10c2dfcb3f068ee7bce
m4st3 <runtycybin@gmail.com>
2024-09-06 17:12:10 +0200

    Register completed

--- src/main/java/com/cybinmvc/demo/controller/RegisterController.java
+++ src/main/java/com/cybinmvc/demo/controller/RegisterController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cybinmvc.demo.model.UsersService;
 import com.cybinmvc.demo.model.Usuario;
@@ -32,12 +33,22 @@ public class RegisterController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<Usuario> signUpUser(
-            @ModelAttribute UsuarioInDto usuario) {
-        log.info("usuario: " + usuario);
-        usersService.insertarUsuario(usuario);
+    public String signUpUser(
+            @ModelAttribute UsuarioInDto usuario,
+            RedirectAttributes redirectAttributes) {
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        log.info("usuario: " + usuario);
+
+        try {
+            usersService.insertarUsuario(usuario);
+            redirectAttributes.addFlashAttribute("message", "Usuario creado");
+            return "redirect:/login";
+        } catch (Exception e) {
+            log.error("ERROR INSERT", "Error insertando " + e.getLocalizedMessage());
+        }
+        
+        redirectAttributes.addFlashAttribute("err", "Error creando usuario");
+        return "redirect/error-register";
     }
 
 }
--- src/main/java/com/cybinmvc/demo/model/Usuario.java
+++ src/main/java/com/cybinmvc/demo/model/Usuario.java
@@ -2,7 +2,9 @@ package com.cybinmvc.demo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +22,8 @@ import lombok.Setter;
 public class Usuario {
 
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = "users_seq", sequenceName = "cybinschema.users_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
     private Long id;
 
     private String email;
